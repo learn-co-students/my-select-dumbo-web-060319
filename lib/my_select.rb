c391ce222041117068e9210e7a8665f0055e2495
Justin Hutsell <hutselljustin@gmail.com>
2019-05-30 05:34:13 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
  # your code here!
+ return nil if collection.empty?
+ 
+ selected = []
+ i = 0 
+ while i < collection.length 
+  selected << collection[i] if yield(collection[i])
+  i += 1
+ end
+ 
+ selected
 end
